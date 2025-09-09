d46e849b467f16afe9365c2e956fc0a00a8b6aef
kistdeov <kistuttidavidka@gmail.com>
2025-09-09 09:33:28 +0200

    Logó javítás3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -89,8 +89,8 @@ const Header = () => {
             <div className="block py-4 lg:py-0">
               <Link href="/">
                 <Image
-                  width={160}
-                  height={32}
+                  width={186}
+                  height={38}
                   src={"/images/logo/Headerlogo_light.png"}
                   alt="Logo"
                   priority
@@ -98,8 +98,8 @@ const Header = () => {
                   style={{ width: "auto", height: "auto" }}
                 />
                 <Image
-                  width={160}
-                  height={32}
+                  width={186}
+                  height={38}
                   src={"/images/logo/Headerlogo.png"}
                   alt="Logo"
                   priority
