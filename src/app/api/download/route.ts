48dbaeeab18b33da77b250aeeac13bfb76e4d512
kistdeov <kistuttidavidka@gmail.com>
2025-09-22 13:17:09 +0200

    Új verzió

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -2,7 +2,7 @@ import { createReadStream } from "fs";
 import path from "path";
 
 export async function GET(req: Request) {
-  const fileUrl = "https://github.com/KistDeov/AiServiceApp/releases/download/1.0.13/aimail-Setup-1.0.13.exe";
+  const fileUrl = "https://github.com/KistDeov/AiServiceApp/releases/download/1.0.14/aimail-Setup-1.0.14.exe";
   return new Response(null, {
     status: 302,
     headers: {
