a7859728b5e18fdf7b8adbda35f30d80f8345017
kistdeov <kistuttidavidka@gmail.com>
2025-09-24 14:47:08 +0200

    Videó link kiszedése

--- src/components/HeroArea/index.tsx
+++ src/components/HeroArea/index.tsx
@@ -41,37 +41,7 @@ const HeroArea = () => {
                     </span>
                   </Link>
 
-                  <Link
-                    href="#"
-                    onClick={() => setToggler(!toggler)}
-                    className="glightbox hover:text-primary dark:hover:text-primary mb-6 inline-flex items-center py-4 text-black dark:text-white"
-                  >
-                    <span className="mr-[22px] flex h-[60px] w-[60px] items-center justify-center rounded-full border-2 border-current">
-                      <svg
-                        width="14"
-                        height="16"
-                        viewBox="0 0 14 16"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M13.5 7.06367C14.1667 7.44857 14.1667 8.41082 13.5 8.79572L1.5 15.7239C0.833334 16.1088 -3.3649e-08 15.6277 0 14.8579L6.05683e-07 1.00149C6.39332e-07 0.231693 0.833334 -0.249434 1.5 0.135466L13.5 7.06367Z"
-                          fill="currentColor"
-                        />
-                      </svg>
-                    </span>
-                    <span className="text-base font-medium">
-                      <span className="block text-sm"> Videó megtekintése </span>
-                      Nézd meg hogyan működik
-                    </span>
-                  </Link>
-
-                  <FsLightbox
-                    toggler={toggler}
-                    sources={[
-                      "https://www.youtube.com/watch?v=_L2sfpTkVHw",
-                    ]}
-                  />
+                  
                 </div>
               </div>
             </div>
