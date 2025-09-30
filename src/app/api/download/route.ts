5dd6e95c1703bc0972f78df5711a261cccbed059
kistdeov <kistuttidavidka@gmail.com>
2025-09-30 14:08:25 +0200

    1.1.2

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -2,7 +2,7 @@ import { createReadStream } from "fs";
 import path from "path";
 
 export async function GET(req: Request) {
-  const fileUrl = "https://github.com/KistDeov/AiServiceApp/releases/download/1.1.1/aimail-Setup-1.1.1.exe";
+  const fileUrl = "https://github.com/KistDeov/AiServiceApp/releases/download/1.1.2/aimail-Setup-1.1.2.exe";
   return new Response(null, {
     status: 302,
     headers: {
