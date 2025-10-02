fab8d260c0fe2cb11b5542037bc2f80f0a713a0d
kistdeov <kistuttidavidka@gmail.com>
2025-10-02 13:05:21 +0200

    statcounter

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { inter } from "@/lib/fonts";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import NextTopLoader from "nextjs-toploader";
 import React from "react";
@@ -45,6 +46,37 @@ export default function RootLayout({
           <ScrollToTop />
         </ThemeProvider>
         <Analytics />
+
+        {/* Statcounter: inline config + external script + noscript fallback */}
+        <Script
+          id="statcounter-inline"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `var sc_project=13168723; var sc_invisible=1; var sc_security="4634ba31";`,
+          }}
+        />
+        <Script
+          src="https://www.statcounter.com/counter/counter.js"
+          strategy="afterInteractive"
+        />
+
+        <noscript>
+          <div className="statcounter">
+            <a
+              title="látogató számláló"
+              href="https://www.statcounter.hu/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="statcounter"
+                src="https://c.statcounter.com/13168723/0/4634ba31/1/"
+                alt="látogató számláló"
+                referrerPolicy="no-referrer-when-downgrade"
+              />
+            </a>
+          </div>
+        </noscript>
       </body>
     </html>
   );
