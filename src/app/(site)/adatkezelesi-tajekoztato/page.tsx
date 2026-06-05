23898c993eb6cf9a82e707e4ed6c6f47b952f9b0
MeggyesiB <meggyesi.bazsi@icloud.com>
2026-06-05 09:26:25 +0200

    adatvéelmi tájékoztató

---
+++ src/app/(site)/adatkezelesi-tajekoztato/page.tsx
@@ -0,0 +1,123 @@
+const privacyNotice = `2018. április 10.
+
+1. Bevezető
+Az szenzor24.hu - a továbbiakban: Weblap - üzemeltetője, a Charter Bt. (Székhely: 7632 Pécs, Kodolányi János utca 25., mailcím: iroda@charter.hu, Cégjegyzékszám: Cg.02-06-062112, Adószám: 20059680-1-02 - a továbbiakban: Adatkezelő), mint Adatkezelő jelen Adatvédelmi Nyilatkozat és Adatkezelési Tájékoztató (a továbbiakban: Adatvédelmi Nyilatkozat) közzétételével ismerteti az adatkezelésére vonatkozó irányelveit, melyeket az Adatkezelő magára nézve kötelező érvényűnek ismer el. Az Adatkezelő minden tőle ésszerűen elvárható intézkedést megtesz az általa kezelt személyes adatok biztonsága érdekében.
+
+Kérjük, hogy Weboldalunk használata előtt szíveskedjen elolvasni a jelen Adatvédelmi Nyilatkozatot, mely közérthető módon tartalmazza, miként kezeljük személyes adatait. Az Adatvédelmi Nyilatkozatban az Adatkezelő egyértelműen és részletesen tájékoztatja az érintetteket az adatok kezelésével kapcsolatos minden fontos tényről.
+
+Az adatkezelés megkezdése előtt tájékoztatjuk Önöket arról, hogy az adatkezelés minden esetben az érintett hozzájárulásán alapul. A Weblapon történő regisztrációjával, a Weblap használatával, megrendelése feladásával és hírlevelünkre történő feliratkozásával Ön, mint Felhasználó kifejezetten hozzájárul ahhoz, hogy az Adatkezelő az Ön személyes adatait jelen Adatvédelmi Nyilatkozat keretein belül, a hatályos magyar jogszabályoknak megfelelően kezelje, és a jelen dokumentumban rögzített terjedelemben, módon és időtartamban felhasználja.
+
+2. Az adatkezelés nyilvántartásba vétele
+Az Adatkezelő a személyes adatok kezelésének nyilvántartásba vételét kérelmezte a Nemzeti Adatvédelmi és Információszabadság Hatóságnál (a továbbiakban: NAIH, vagy Hatóság).
+
+3. Fogalom meghatározások
+A Felhasználók személyes adataik kezelésének szabályait tartalmazó hatályos jogszabály: 2011. évi CXII. törvény - az információs önrendelkezési jogról és az információszabadságról (a továbbiakban: Infotv.). A jelen Adatvédelmi Nyilatkozatban használt fogalmak jelentése a törvény 3. §-ban rögzített értelmező rendelkezések figyelembe vételével az alábbi:
+
+1. személyes adat: az érintettel kapcsolatba hozható adat - különösen az érintett neve, azonosító jele, valamint egy vagy több fizikai, fiziológiai, mentális, gazdasági, kulturális vagy szociális azonosságára jellemző ismeret -, valamint az adatból levonható, az érintettre vonatkozó következtetés
+2. érintett: bármely meghatározott, személyes adat alapján azonosított vagy - közvetlenül vagy közvetve - azonosítható természetes személy
+3. adatkezelő: az a természetes vagy jogi személy, illetve jogi személyiséggel nem rendelkező szervezet, aki, vagy amely önállóan vagy másokkal együtt az adat kezelésének célját meghatározza, az adatkezelésre vonatkozó döntéseket meghozza és végrehajtja, vagy az adatfeldolgozóval végrehajtatja
+4. adatkezelés: az alkalmazott eljárástól függetlenül az adaton végzett bármely művelet vagy a műveletek összessége, így különösen gyűjtése, felvétele, rögzítése, rendszerezése, tárolása, megváltoztatása, felhasználása, lekérdezése, továbbítása, nyilvánosságra hozatala, összehangolása vagy összekapcsolása, zárolása, törlése és megsemmisítése, valamint az adat további felhasználásának megakadályozása, fénykép-, hang- vagy képfelvétel készítése, valamint a személy azonosítására alkalmas fizikai jellemzők rögzítése
+5. adattörlés: az adat felismerhetetlenné tétele oly módon, hogy a helyreállítása többé nem lehetséges
+6. adatfeldolgozás: az adatkezelési műveletekhez kapcsolódó technikai feladatok elvégzése, függetlenül a műveletek végrehajtásához alkalmazott módszertől és eszköztől, valamint az alkalmazás helyétől, feltéve hogy a technikai feladatot az adaton végzik
+7. adatállomány: az egy nyilvántartásban kezelt adatok összessége
+8. harmadik személy: olyan természetes vagy jogi személy, illetve jogi személyiséggel nem rendelkező szervezet, aki vagy amely nem azonos az érintettel, az adatkezelővel vagy az adatfeldolgozóval
+9. információs társadalommal összefüggő szolgáltatás: elektronikus úton, távollevők részére, rendszerint ellenszolgáltatás fejében nyújtott szolgáltatás, amelyhez a szolgáltatás igénybe vevője egyedileg fér hozzá
+10. elektronikus kereskedelmi szolgáltatás: olyan információs társadalommal összefüggő szolgáltatás, amelynek célja valamely birtokba vehető forgalomképes ingó dolog értékesítése, beszerzése, cseréje vagy más módon történő igénybevétele
+
+4. Felhasználók köre
+Felhasználó egyaránt az oldalon regisztrált, valamint a nem regisztrált, de a Weblap szolgáltatásait igénybevevő, rendelést leadó, bármely meghatározott, személyes adat alapján azonosított vagy - közvetlenül vagy közvetve - azonosítható természetes személy.
+
+5. A kezelt személyes adatok köre
+Az Adatkezelő által kezelt személyes adatok körét képezik az alábbiak:
+
+Név (Vezetéknév, Keresztnév, Vállalkozás neve)
+Cím (Utca, házszám, emelet, ajtó, város, ország, irányítószám)
+Telefonszám
+E-mail cím
+
+Az Adatkezelő a kezelt személyes adatok valódiságáért felelősséget nem vállal. A szolgáltatott személyes adatokért a felelősség kizárólag a Felhasználót terheli.
+
+6. Hírlevél
+Természetes személynek, mint a hírlevelünk címzettjének elektronikus levelezés vagy azzal egyenértékű más egyéni kommunikációs eszköz útján hírlevelet kizárólag akkor küldünk, ha ahhoz a hírlevél címzettje előzetesen egyértelműen és kifejezetten hozzájárult, a hírlevélre feliratkozott.
+
+A kezelt adatok köre: A hírlevélre önkéntesen, kifejezett hozzájárulása alapján feliratkozott Felhasználó alábbi adatait kezeli az Adatkezelő: Név (Vezetéknév, Keresztnév, Vállalkozás neve), E-mail cím.
+
+A felhasználás célja: Marketing tartalmú elektronikus levél küldése (hírlevél) elektronikus úton a hírlevélre önkéntesen, kifejezett hozzájárulás alapján feliratkozott Felhasználók részére; a Felhasználók tájékoztatása aktualitásokról, ajánlatokról, marketing célú akciókról.
+
+Az adatkezelés időtartama: Az Adatkezelő a hírlevélre önkéntesen feliratkozó felhasználó fent nevesített személyes adatait a felhasználó hozzájárulásának visszavonásáig kezeli, ennek hiányában az adatok felvételétől számított 10 évig.
+
+Visszavonó nyilatkozat: Tájékoztatjuk az olvasót arról, hogy a hozzájáruló nyilatkozat bármikor korlátozás és indokolás nélkül, ingyenesen visszavonható. Ebben az esetben a nyilatkozó nevét és minden egyéb személyes adatát töröljük, és részére hírlevelet a továbbiakban nem küldünk. A visszavonó nyilatkozat megtételére mind postai úton, mind pedig elektronikus levél útján lehetőséget biztosítunk úgy, hogy a nyilatkozatot tevő személy egyértelműen azonosítható legyen.
+
+Az Adatkezelő az általa rögzített személyes adatokat csak a hozzájáruló nyilatkozatban foglaltaknak megfelelően kezeli és harmadik fél számára kizárólag az érintett személy előzetes hozzájárulásával adhatja át.
+
+7. A személyes adatok kezelésének célja és jogalapja
+Adatkezelési célok: Az adatok kezelésének célja az Adatkezelő által nyújtott szolgáltatások Felhasználók általi igénybevételének, és az Adatkezelő általi teljesítésének lehetővé tétele, a termékértékesítés nyomon követése, a Felhasználó azonosítása, valamint annak lehetővé tétele, hogy a Felhasználó a rendeléséről tájékoztatást kapjon.
+
+A Weblap szolgáltatásainak igénybevétele regisztrációhoz, illetőleg személyes adatok megadásához kötött. A szolgáltatás igénybevételével és a Weblapon történő regisztrációval a Felhasználó kifejezetten hozzájárul, hogy az Adatkezelő személyes adatait kezelje. Személyes adatait a Felhasználó önkéntesen, jelen Adatkezelési Nyilatkozat ismeretében bocsájtja az Adatkezelő rendelkezésére.
+
+Az adatkezelés jogalapja és módja: A Weblapon történő regisztráció során, valamint a szolgáltatás igénybevétele folytán történő személyes adatok rögzítése és kezelése a Felhasználó önkéntes hozzájárulásán alapul. Ezen hozzájárulást a Felhasználó a Weblapon történő rendelés leadásával, regisztráció elvégzésével adja meg.
+
+A Felhasználó, ha szolgáltatás igénybevétele során harmadik, természetes személy személyes adatait teszi hozzáférhetővé, szavatol azért, hogy a hozzáférhetővé tett személyes adatok kezeléséhez az érintett hozzájárulását jogszerűen beszerezte.
+
+8. Az adatkezelésre és adatfeldolgozásra jogosult személy
+A személyes adatok kezelésére és feldolgozására jogosult személy (az 1. pontban meghatározott szervezet - a továbbiakban: Adatkezelő), mint Adatkezelő. A feldolgozásra kerülő személyes adatokat az Adatkezelő mindenkori törvényes képviselője(i), alkalmazottai/megbízottai/közreműködői ismerhetik meg. Az adatkezelő személyes adatot harmadik személyek részére nem ad át, kizárólag abban az esetben, ha az érintett ehhez kifejezetten hozzájárul.
+
+9. Az adatkezelés időtartama
+A Felhasználó hozzájárulása alapján megadott személyes adatokat az Adatkezelő az adatkezelés céljának megvalósulásáig, illetőleg a Felhasználó hozzájárulása visszavonásáig kezeli. Az Adatkezelő a Felhasználó regisztrációja során megadott személyes adatait a Weblap igénybevételének megszűnéséig, így különösen a regisztráció törléséig kezeli.
+
+Az Adatkezelő a felvett személyes adatokat törvény eltérő rendelkezésének hiányában a) a rá vonatkozó jogi kötelezettség teljesítése céljából, vagy b) az adatkezelő vagy harmadik személy jogos érdekének érvényesítése céljából, ha ezen érdek érvényesítése a személyes adatok védelméhez fűződő jog korlátozásával arányban áll - további külön hozzájárulás nélkül, valamint az érintett hozzájárulásának visszavonását követően is kezelheti. (2011. évi CXII. törvény 6. § (5))
+
+Az Adatkezelő a Felhasználó által megadott személyes adatokat a számviteli kötelezettségek teljesítése érdekében a 2000. évi C. törvény 169. §-a alapján 8 évig, illetve az adózás rendjéről szóló 2003. évi XCII. törvényben meghatározott elévülési időn belül őrzi meg és kezeli.
+
+10. Adattovábbítás
+Az Adatkezelő nem ad el, nem ad bérbe és semmilyen formában nem bocsájt rendelkezésre a Felhasználóra vonatkozó személyes adatokat illetve információkat más cégnek, magánszemélynek, kivéve a számviteli kötelezettség teljesítéséhez szükséges adatszolgáltatást.
+
+Az Adatkezelő a tőle elvárható módon gondoskodik az adatok megfelelő biztonságáról, és megteszi azokat a technikai és szervezési intézkedéseket, melyek az adatvédelmi szabályok és elvek érvényre juttatását garantálják és a személyes adatok biztonságát elősegítik. Tájékoztatjuk a Tisztelt Felhasználókat arról, hogy az Adatkezelő személyes adatot kizárólag az érintett hozzájárulásával továbbít harmadik személynek vagy személyeknek.
+
+Az adatkezelés időtartama: az adatkezelés céljának megvalósulásáig, illetőleg a Felhasználó hozzájárulása visszavonásáig kezeli, ennek hiányában az adatok felvételétől számított 10 évig.
+
+Az adattovábbítás jogalapja: az érintett hozzájárulásán alapul.
+
+11. "Cookie"-k
+Weboldalunk a Felhasználó egyénre szabott kiszolgálása, és a kényelmi funkciók érdekében "cookie"-kat használhat. "Cookie"-k használata esetén ennek keretében a Weblapra történő látogatásakor a látogatás időpontja, a Felhasználó böngészőjének adatai, IP címe valamint a használt operációs rendszer és a nyelvi beállítások naplózásra kerülnek. Ezen adatokat az Adatkezelő nem kapcsolja össze más személyes adatokkal, kizárólag statisztikai kimutatások célját szolgálják.
+
+12. Felhasználók jogai és jogorvoslati lehetőségek
+A Felhasználónak lehetősége van az Adatkezelőnél személyes adatai kezeléséről tájékoztatást kérnie, ezen kívül a Felhasználó kérheti személyes adatainak helyesbítését, valamint személyes adatainak törlését, vagy zárolását.
+
+A Felhasználó bármikor jogosult írásban, az Adatkezelő által a jelen Adatvédelmi Nyilatkozatban megjelölt elérhetőségein tájékoztatást kérni az Adatkezelő által kezelt, reá vonatkozó személyes adatokról, valamint ugyanígy bármikor kérelmezheti azok módosítását. Az Adatkezelő a kérelem benyújtásától számított legfeljebb 30 napon belül adja meg a kért tájékoztatást.
+
+Az Adatkezelő ezen kívül, ilyen irányú kérelem alapján mail-ben tájékoztatást nyújt az általa kezelt személyes adatok forrásáról, az adatkezelés céljáról, jogalapjáról, időtartamáról, az adatfeldolgozó nevéről, címéről és az adatkezeléssel összefüggő tevékenységéről, az adatvédelmi incidens körülményeiről, hatásairól és az elhárítására megtett intézkedésekről, továbbá - az érintett személyes adatainak továbbítása esetén - az adattovábbítás jogalapjáról és címzettjéről.
+
+A Felhasználó bármikor jogosult a helytelen, megváltozott vagy hibásan rögzített adatainak kijavítását, vagy azok törlését kérni. Az Adatkezelő a kérelem beérkezését követően ésszerű időn belül, de legkésőbb a kérelem beérkezésétől számított 30 napon belül törli, illetve módosítja a Felhasználó által megjelölt adatokat.
+
+Törlés helyett az adatkezelő zárolja a személyes adatot, ha az érintett ezt kéri, vagy ha a rendelkezésére álló információk alapján feltételezhető, hogy a törlés sértené az érintett jogos érdekeit. Az így zárolt személyes adat kizárólag addig kezelhető, ameddig fennáll az az adatkezelési cél, amely a személyes adat törlését kizárta.
+
+A Felhasználó a személyes adatok kezelésével kapcsolatos jogsérelem esetén az információs önrendelkezési jogról és az információszabadságról szóló 2011. évi CXII. törvény alapján a Nemzeti Adatvédelmi és Információszabadság Hatósághoz (NAIH) (Cím: 1125 Budapest, Szilágyi Erzsébet fasor 22/C, Levelezési cím: 1530 Budapest, Postafiók: 5.) fordulhat, végső soron bíróság előtt érvényesítheti jogait.
+
+14. Jogszabályoknak való megfelelés
+A jelen Adatvédelmi Nyilatkozat elkészítése során, valamint a Felhasználók személyes adatainak kezelésekor az Adatkezelő a mindenkor hatályos magyar jogszabályok maradéktalan betartásával, és a vonatkozó uniós normák figyelembevételével jár el. Az Adatkezelő különösen az alábbi jogszabályokban előírtakat tartja szem előtt az adatkezelés során:
+
+2011. évi CXII. törvény - az információs önrendelkezési jogról és az információszabadságról (Infotv.)
+2001. évi CVIII. törvény - az elektronikus kereskedelmi szolgáltatások, valamint az információs társadalommal összefüggő szolgáltatások egyes kérdéseiről (főképp a 13/A. §-a)
+2008. évi XLVII. törvény - a fogyasztókkal szembeni tisztességtelen kereskedelmi gyakorlat tilalmáról
+2008. évi XLVIII. törvény - a gazdasági reklámtevékenység alapvető feltételeiről és egyes korlátairól (különösen a 6. §-a)
+2005. évi XC. törvény az elektronikus információszabadságról
+2003. évi C. törvény az elektronikus hírközlésről (kifejezetten a 155. §-a)
+16/2011. sz. vélemény a viselkedésalapú online reklám bevált gyakorlatára vonatkozó EASA/IAB-ajánlásról`;
+
+export default function AdatkezelesiTajekoztatoPage() {
+  return (
+    <section className="relative z-10 py-24">
+      <div className="container">
+        <div className="mx-auto max-w-5xl rounded-2xl border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-[#1f233a] sm:p-8">
+          <h1 className="mb-6 text-3xl font-bold text-black dark:text-white">
+            Adatkezelési Tájékoztató
+          </h1>
+          <div className="whitespace-pre-wrap text-sm leading-7 text-gray-700 dark:text-gray-200">
+            {privacyNotice}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
